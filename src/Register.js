1db1c97e9a4a217d1919fa1f1631c1f52a19cd6e
abdul.rasheed <abdulrasheed.techy@gmail.com>
2024-06-06 22:37:02 +0530

    feat - register and login view

---
+++ src/Register.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+export default function Register() {
+    return (
+        <>
+            <h2>Register</h2>
+            <Form>
+                {/* email */}
+                <Form.Group controlId='formBasicEmail'>
+                    <Form.Label> Email </Form.Label>
+                    <Form.Control type='email' placeholder='Email'/> 
+                </Form.Group>
+                {/* password */}
+                <Form.Group controlId='formBasicPassword'>
+                    <Form.Label> Password </Form.Label>
+                    <Form.Control type='password' placeholder='Password'/> 
+                </Form.Group>
+                {/*submit button */}
+                <Button variant='primary' type='submit'>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
+}
